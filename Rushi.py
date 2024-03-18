78323e8db158b6c2e339590dfda8cef129a49ea1
Pratika1012 <118338761+Pratika1012@users.noreply.github.com>
2024-03-18 17:27:19 +0530

    Update Rushi.py

--- Rushi.py
+++ Rushi.py
@@ -7,7 +7,7 @@ from fpdf import FPDF
 import google.generativeai as genai
 from datetime import datetime, timedelta
 from io import BytesIO
-import pdfkit
+
 
 st.set_page_config(layout="wide")
 
@@ -24,13 +24,6 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config)
-
-generation_config2 = {
-  "temperature": 0.6,
-  "top_p": 1,
-  "top_k": 1,
-  "max_output_tokens": 4096,
-}
 
 def generate_pdf(response_text):
     # Create a PDF document
@@ -50,29 +43,44 @@ def generate_pdf(response_text):
     st.markdown(href, unsafe_allow_html=True)
 
 
-model2 = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config2)
 challenge = "RSS Feeds for Competitive Intelligence and Summarization"
 objective = "Stay ahead of competitors by monitoring industry trends"
 analysis = "Competitive Intelligence Analysis"
-# Streamlit UI
-st.markdown("<h1 style='text-align: center;'>Competitive Intelligence Analysis</h1>", unsafe_allow_html=True)
 
-st.sidebar.header("ITC")
+
+st.sidebar.header("Competitive Intelligence Analysis")
 industry = st.sidebar.text_input("Industry Name","Pharmaceutical")
 market = st.sidebar.text_input("Market/Geography Name", "United States")
 client = st.sidebar.text_input("Client Name", "Astrazeneca")
 curr_date = st.sidebar.date_input("Fortnight End Date ", format="DD/MM/YYYY")
+temperature= st.sidebar.slider('Generated Text (Deterministic <===> Creative)', 0.0, 0.5, 0.25)
+top_k = st.sidebar.slider('Vocabulary Diversity (Deterministic <===> Creative)', 1, 50, 25)
+
+generation_config2 = {
+  "temperature": temperature,
+  "top_p": 1,
+  "top_k": top_k,
+  "max_output_tokens": 4096,
+}
+model2 = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config2)
 
 if st.sidebar.button("Generate Analysis"):
+
+    days_to_subtract = timedelta(days=15)
+    past_date = curr_date - days_to_subtract
+    desired_format = "%d-%m-%Y"
+    curr_date = curr_date.strftime(desired_format)
+    past_date = past_date.strftime(desired_format)
+
+    # Streamlit UI
+    st.markdown(f"<h2 style='text-align: center;'>Fortnightly Newsletter</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h3 style='text-align: center;'>Competitive Intelligence Analysis for {client}</h3>", unsafe_allow_html=True)
+    st.markdown(f"<h3 style='text-align: center;'>{industry} industry in {market}</h3>", unsafe_allow_html=True)
     # Convert date string to date object
     # date_format = "%d-%m-%y"
     # date_object = datetime.strptime(current_date, date_format)
     # curr_date = date_object.date()
-    days_to_subtract = timedelta(days=15)
-    past_date = curr_date - days_to_subtract
-    # desired_format = "%d-%m-%Y"
-    # curr_date = curr_date.strftime(desired_format)
-    # past_date = past_date.strftime(desired_format)
+    
 
     # Generate content using Generative Models
     # competitor_list = f"For {industry} industry, {market} market, {client} client, give a list of competitors."
@@ -124,29 +132,26 @@ if st.sidebar.button("Generate Analysis"):
             Tailor content for manager level audience. &\
             Ensure information adheres to ethical and legal boundaries."
 
-    notes2 = f"Output should be in following format: &\
-            Headline: Competitive Edge: Key Insights for the Fortnight of {past_date} - {curr_date} &\
-            Introduction: Briefly welcome readers and introduce the purpose  of the newsletter. &\
-            Provide date, source info name and source url for each item in the body. &\
-            Body: &\
-            Competitive Landscape Overview: Summarize key industry trends and developments. &\
-            Competitor Spotlight: A few lines related to any recent developments for major competitors. Try including as many points as possible. &\
-            Recent News & Announcements: Highlight significant news, launches, partnerships, or campaigns throughout the industry. Try including as many points as possible. &\
-            Financial Performance & Market Share: Analyze data on competition's financial performance and Industry market share shifts. Try including as many points as possible. &\
-            Strategic Moves & Insights: Identify potential strategic moves by competitors and analyze their impact on {client} business. Try including as many points as possible. &\
-            Actionable Insights: Provide key takeaways for {client} company to maintain a competitive edge. Try including as many points as possible. &\
-            Conclusion: Encourage feedback and next edition information."
+    notes2 = f"Output should be in following layout: &\
+          Headline: Key Insights for the Fortnight of {past_date} to {curr_date} &\
+          Introduction: Briefly welcome readers and introduce the purpose  of the newsletter. &\
+          Provide date, source info name and source url for each item in the body. &\
+          Body: &\
+          Competitive Landscape Overview: Mention key industry trends and developments while expanding upon the basis of the article in depth. Leave two lines space after each section.&\
+          Competitor Spotlight: A few lines related to any recent developments for major competitors. Try including as many points as possible. &\
+          Recent News & Announcements: Highlight significant news, launches, partnerships, or campaigns throughout the industry. Try including as many points as possible. &\
+          Financial Performance & Market Share: Analyze data on competition's financial performance and Industry market share shifts. Try including as many points as possible. &\
+          Strategic Moves & Insights: Identify potential strategic moves by competitors and analyze their impact on {client} business. Try including as many points as possible. &\
+          Actionable Insights: Provide key takeaways for {client} company to maintain a competitive edge. Try including as many points as possible. &\
+          Conclusion: Encourage feedback and next edition information. &\
+          Design the output in a beautiful format using san serif font. Word count 1500 words - 2000 words."
 
     prompt_parts = [objective,analysis,analysis_RSS,notes,notes2]
     response = model2.generate_content(prompt_parts)
+    st.write(response.text)
     response_text=response.text
-    st.write(response_text)
-    
+
     response_without_asterisks = response_text.replace('*', '')
     generate_pdf(response_without_asterisks)
-
-  
-
-
     
-
+    
