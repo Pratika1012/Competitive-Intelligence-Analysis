629a72515298c1906b420d514891ad757f523346
Pratika1012 <118338761+Pratika1012@users.noreply.github.com>
2025-04-24 13:20:42 +0530

    Update Rushi.py

--- Rushi.py
+++ Rushi.py
@@ -1,118 +1,141 @@
+
 import streamlit as st
 import pandas as pd
-from groq import Groq
+import google.generativeai as genai
 from datetime import datetime, timedelta
 import base64
 from fpdf import FPDF
+import google.generativeai as genai
+from datetime import datetime, timedelta
 from io import BytesIO
+
 
 st.set_page_config(layout="wide")
 
-# Configure Groq API
-groq_api_key = st.secrets.get("GROQ_API_KEY", "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")  # Replace with your Groq API key
-try:
-    client = Groq(api_key=groq_api_key)
-    if not hasattr(client, 'chat') or not hasattr(getattr(client, 'chat'), 'completions'):
-        st.error("Groq client initialization failed: Invalid API key or unsupported method.")
-        st.stop()
-except Exception as e:
-    st.error(f"Failed to initialize Groq client: {str(e)}")
-    st.stop()
+# Configure Google Generative AI
+gemini_api_key = "AIzaSyAGxhWaMKBEnFXfhGeQWWvq92rE5JtkTIw"
+genai.configure(api_key=gemini_api_key)
 
-# Define generation configuration
+# Define Generative Models
 generation_config = {
-    "temperature": 0.0,
-    "max_tokens": 4096,
-    "top_p": 1.0,
+  "temperature": 0,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 4096,
 }
 
-def generate_content(prompt, config):
-    """Helper function to generate content using Groq API."""
-    try:
-        response = client.chat.completions.create(
-            model="llama-3.1-70b-versatile",  # Ensure this model is available
-            messages=[{"role": "user", "content": prompt}],  # Chat format with a single user message
-            temperature=config["temperature"],
-            max_tokens=config["max_tokens"],
-            top_p=config["top_p"]
-        )
-        return response.choices[0].message.content  # Extract the generated text
-    except Exception as e:
-        st.error(f"Error generating content: {str(e)}")
-        return None
+model = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config)
 
 def generate_pdf(response_text):
+    # Create a PDF document
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
+
+    # Add the response_text to the PDF document
     pdf.multi_cell(0, 10, response_text)
+
+    # Save the PDF document to a BytesIO object
     pdf_buffer = pdf.output(dest='S').encode('latin1')
     b64_pdf = base64.b64encode(pdf_buffer).decode()
+
+    # Provide a download link for the generated PDF
     href = f'<a href="data:application/pdf;base64,{b64_pdf}" download="Generated_Analysis.pdf">Download PDF</a>'
     st.markdown(href, unsafe_allow_html=True)
+
 
 challenge = "RSS Feeds for Competitive Intelligence and Summarization"
 objective = "Stay ahead of competitors by monitoring industry trends"
 analysis = "Competitive Intelligence Analysis"
 
+
 st.sidebar.header("Competitive Intelligence Analysis")
-industry = st.sidebar.text_input("Industry Name", "Pharmaceutical")
+industry = st.sidebar.text_input("Industry Name","Pharmaceutical")
 market = st.sidebar.text_input("Market/Geography Name", "United States")
-client_name = st.sidebar.text_input("Client Name", "Astrazeneca")
-curr_date = st.sidebar.date_input("Fortnight End Date", format="DD/MM/YYYY")
-temperature = st.sidebar.slider('Generated Text (Deterministic <===> Creative)', 0.0, 0.5, 0.25)
-top_p = st.sidebar.slider('Vocabulary Diversity (Deterministic <===> Creative)', 0.1, 1.0, 0.9)
+client = st.sidebar.text_input("Client Name", "Astrazeneca")
+curr_date = st.sidebar.date_input("Fortnight End Date ", format="DD/MM/YYYY")
+temperature= st.sidebar.slider('Generated Text (Deterministic <===> Creative)', 0.0, 0.5, 0.25)
+top_k = st.sidebar.slider('Vocabulary Diversity (Deterministic <===> Creative)', 1, 50, 25)
 
 generation_config2 = {
-    "temperature": temperature,
-    "max_tokens": 4096,
-    "top_p": top_p,
+  "temperature": temperature,
+  "top_p": 1,
+  "top_k": top_k,
+  "max_output_tokens": 4096,
 }
+model2 = genai.GenerativeModel(model_name="gemini-pro", generation_config=generation_config2)
 
 if st.sidebar.button("Generate Analysis"):
+
     days_to_subtract = timedelta(days=15)
     past_date = curr_date - days_to_subtract
     desired_format = "%d-%m-%Y"
-    curr_date_str = curr_date.strftime(desired_format)
-    past_date_str = past_date.strftime(desired_format)
+    curr_date = curr_date.strftime(desired_format)
+    past_date = past_date.strftime(desired_format)
 
+    # Streamlit UI
     st.markdown(f"<h2 style='text-align: center;'>Fortnightly Newsletter</h2>", unsafe_allow_html=True)
     st.markdown(f"<h3 style='text-align: center;'>Competitive Intelligence Analysis for {client}</h3>", unsafe_allow_html=True)
     st.markdown(f"<h3 style='text-align: center;'>{industry} industry in {market}</h3>", unsafe_allow_html=True)
+    # Convert date string to date object
+    # date_format = "%d-%m-%y"
+    # date_object = datetime.strptime(current_date, date_format)
+    # curr_date = date_object.date()
+    
+
+    # Generate content using Generative Models
+    # competitor_list = f"For {industry} industry, {market} market, {client} client, give a list of competitors."
+    # prompt_parts = [competitor_list]
+    # response_comp = model.generate_content(prompt_parts)
+
+    # RSS_feed1 = f"For {industry} industry, {market} market, compile a list of relevant RSS feed URLs."
+    # RSS_feed2 = f"For {industry} industry, {market} market, {client} client and {response_comp.text} competitors, compile a list of relevant RSS feed URLs."
+    
+    # prompt_parts = ["Competitive Intelligence Analysis", "Stay ahead of competitors by monitoring industry trends", RSS_feed1]
+    # response1 = model.generate_content(prompt_parts)
+
+    # prompt_parts = ["Competitive Intelligence Analysis", "Stay ahead of competitors by monitoring industry trends", RSS_feed2]
+    # response2 = model.generate_content(prompt_parts)
+
+    # prompt_parts = ["Combine two responses.", response1.text, response2.text]
+    # final_response = model.generate_content(prompt_parts)
+
+    # st.subheader("Generated Competitive Intelligence Analysis")
+    # st.write(final_response.text)
+
+# st.markdown(footer, unsafe_allow_html=True)
+    
+    company_info = f"Summarize company information {client}"
 
     competitor_list = f"For {industry} industry, {market} market, {client} client, give a list of competitors."
-    response_comp = generate_content(competitor_list, generation_config)
-    if response_comp is None:
-        st.stop()
+
+    prompt_parts = [competitor_list]
+    response_comp = model.generate_content(prompt_parts)
 
     RSS_feed1 = f"For {industry} industry, {market} market, compile a list of relevant RSS feed URLs."
-    RSS_feed2 = f"For {industry} industry, {market} market, {client} client and {response_comp} competitors, compile a list of relevant RSS feed URLs."
+    RSS_feed2 = f"For {industry} industry, {market} market, {client} client and {response_comp.text} competitors, compile a list of relevant RSS feed URLs."
 
-    prompt1 = f"{analysis}\n{objective}\n{RSS_feed1}"
-    response1 = generate_content(prompt1, generation_config)
-    if response1 is None:
-        st.stop()
+    prompt_parts = [analysis,objective,RSS_feed1]
+    response1 = model.generate_content(prompt_parts)
 
-    prompt2 = f"{analysis}\n{objective}\n{RSS_feed2}"
-    response2 = generate_content(prompt2, generation_config)
-    if response2 is None:
-        st.stop()
+    prompt_parts = [analysis,objective,RSS_feed2]
+    response2 = model.generate_content(prompt_parts)
 
-    combine_prompt = f"Combine two responses.\nResponse 1: {response1}\nResponse 2: {response2}"
-    combined_response = generate_content(combine_prompt, generation_config)
-    if combined_response is None:
-        st.stop()
+    prompt_parts = ["Combine two responses.",response1.text,response2.text]
+    response = model.generate_content(prompt_parts)
 
-    analysis_RSS = f"For {industry} industry, {market} market, {client_name} client...", perform detailed competitive intelligence analysis by fetching data from {combined_response}"
+    #analysis = f"For {industry} industry in {market}, perform {analysis_model} analysis"
+    analysis_RSS = f"For {client} client, {industry} industry, {market} market, perform detailed competitive intelligence analysis by fetching data from {response.text}"
 
-    notes = f"Analysis should be elaborate in nature and should present statistics and latest data. Only focus on developments between {past_date_str} to {curr_date_str}. Do not use old articles and data. &\
-            Analyse their data and prepare the output accordingly. Include visuals for better understanding. Briefly cite sources for information. &\
+    #notes2 = "Industry news and trending topics. Competitor news (i.e. financial reporting, new partnerships, mergers & acquisitions, and product announcements). Upcoming events, webinars, and conferences. Industry thought leadership pieces."
+    notes = f"Analysis should be elaborate in nature and should present statistics and latest data. Only focus on developments between {past_date} to {curr_date}. Do not use old articles and data. &\
+            Analyse their data and prepare the output accordingly. Include visuals for better understanding.  Briefly cite sources for information. &\
             Tailor content for manager level audience. &\
             Ensure information adheres to ethical and legal boundaries."
 
     notes2 = f"Output should be in following layout: &\
-          Headline: Key Insights for the Fortnight of {past_date_str} to {curr_date_str} &\
-          Introduction: Briefly welcome readers and introduce the purpose of the newsletter. &\
+          Headline: Key Insights for the Fortnight of {past_date} to {curr_date} &\
+          Introduction: Briefly welcome readers and introduce the purpose  of the newsletter. &\
           Provide date, source info name and source url for each item in the body. &\
           Body: &\
           Competitive Landscape Overview: Mention key industry trends and developments while expanding upon the basis of the article in depth. Leave two lines space after each section.&\
@@ -124,13 +147,12 @@ if st.sidebar.button("Generate Analysis"):
           Conclusion: Encourage feedback and next edition information. &\
           Design the output in a beautiful format using san serif font. Word count 1500 words - 2000 words."
 
-    final_prompt = f"{objective}\n{analysis}\n{analysis_RSS}\n{notes}\n{notes2}"
-    final_response = generate_content(final_prompt, generation_config2)
-    if final_response is None:
-        st.stop()
-
-    st.write(final_response)
-    response_text = final_response
+    prompt_parts = [objective,analysis,analysis_RSS,notes,notes2]
+    response = model2.generate_content(prompt_parts)
+    st.write(response.text)
+    response_text=response.text
 
     response_without_asterisks = response_text.replace('*', '')
     generate_pdf(response_without_asterisks)
+    
+    
